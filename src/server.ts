6ca37c45bb1acb87b0efb022d08b2b648e7e6024
Moisés Santana <moisaant@gmail.com>
2023-09-07 02:13:47 -0300

    fix: add host in server

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import { env } from './env'
 
 app
   .listen({
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log('Server is running on port 3333')
+    console.log('HTTP Server Running! 🚀')
   })
